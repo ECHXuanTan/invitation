9d433c58385e9877ff6a40ca247dedebe22580d7
Duong Hai Pham <duonghaipham2001@gmail.com>
2023-11-06 11:44:26 +0700

    Update time

--- src/features/Card/index.jsx
+++ src/features/Card/index.jsx
@@ -26,10 +26,10 @@ export default function Card() {
             <HiOutlineAcademicCap className="w-20 h-20 text-gray-400" />
           </div>
           <p className="mb-2">{t("card.on").toUpperCase()}</p>
-          <p className="mb-10 text-4xl">28 / 12 / 2023</p>
+          <p className="mb-10 text-4xl">{t("card.thu") 16 / 11 / 2023</p>
           <p className="mb-10">
-            {t("card.from").toUpperCase()} 8:30 {t("card.to").toUpperCase()}{" "}
-            11:00
+            {t("card.from").toUpperCase()} 11:00 {t("card.to").toUpperCase()}{" "}
+            14:00
           </p>
           <p>
             {t("card.at").toUpperCase()}
