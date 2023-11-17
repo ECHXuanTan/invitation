401ebe002d85aab9d71ca1f696aa826c0cbac7ed
Duong <duonghaipham2001@gmail.com>
2023-11-17 12:04:09 +0700

    Add photos for gallery

--- src/features/Gallery/index.jsx
+++ src/features/Gallery/index.jsx
@@ -6,12 +6,32 @@ export default function Gallery() {
   const { t } = useTranslation();
 
   const [srcSet] = useState([
-    "/assets/images/mock-01.png",
-    "/assets/images/mock-02.png",
-    "/assets/images/mock-03.png",
-    "/assets/images/mock-04.png",
-    "/assets/images/mock-05.png",
-    "/assets/images/mock-06.png",
+    "/assets/images/00a.jpg",
+    "/assets/images/00b.jpg",
+    "/assets/images/01.jpg",
+    "/assets/images/02.jpg",
+    "/assets/images/03.jpg",
+    "/assets/images/04.jpg",
+    "/assets/images/05.jpg",
+    "/assets/images/06.jpg",
+    "/assets/images/07.jpg",
+    "/assets/images/08.jpg",
+    "/assets/images/09.jpg",
+    "/assets/images/10.jpg",
+    "/assets/images/11.jpg",
+    "/assets/images/12.jpg",
+    "/assets/images/13.jpg",
+    "/assets/images/14.jpg",
+    "/assets/images/15.jpg",
+    "/assets/images/16.jpg",
+    "/assets/images/17.jpg",
+    "/assets/images/18.jpg",
+    "/assets/images/19.jpg",
+    "/assets/images/20.jpg",
+    "/assets/images/21.jpg",
+    "/assets/images/22.jpg",
+    "/assets/images/23.jpg",
+    "/assets/images/24.jpg",
   ]);
 
   return (
@@ -19,7 +39,13 @@ export default function Gallery() {
       <Title blurredText={t("gallery.photos")} text={t("gallery.title")} />
       <div className="columns-1 sm:columns-3 gap-4 space-y-4">
         {srcSet.map((src, index) => (
-          <img key={index} src={src} alt="gallery" className="w-full" />
+          <img
+            key={index}
+            src={src}
+            alt="gallery"
+            className="w-full"
+            loading="lazy"
+          />
         ))}
       </div>
     </section>
